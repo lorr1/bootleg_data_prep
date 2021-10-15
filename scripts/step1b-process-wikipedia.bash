c97b2effc9c1571c42a2551be1e007bd0c5612b8
rubmz <rubi@rubmz.com>
2021-10-15 14:17:27 +0300

    forgot to remove debuggin flag in script

--- scripts/step1b-process-wikipedia.bash
+++ scripts/step1b-process-wikipedia.bash
@@ -1,3 +1,3 @@
 source ./envs.bash
 cd $BOOTLEG_WIKIPEDIA_DIR
-python3 -m bootleg_data_prep.wikiextractor.WikiExtractor --output sentences --output2 pageids $BOOTLEG_WIKIPEDIA_DUMP_FILENAME --processes $BOOTLEG_PROCESS_COUNT --short_debug
+python3 -m bootleg_data_prep.wikiextractor.WikiExtractor --output sentences --output2 pageids $BOOTLEG_WIKIPEDIA_DUMP_FILENAME --processes $BOOTLEG_PROCESS_COUNT

635526693cb88a9ce4b7b754c483fb7d1acf2a66
lorr1 <57237365+lorr1@users.noreply.github.com>
2021-12-09 13:41:32 -0800

    Ahh. Small fix to paths in generate slices

--- scripts/step7c-generate-slices.bash
+++ scripts/step7c-generate-slices.bash
@@ -9,13 +9,13 @@ python3 $BOOTLEG_PREP_CODE_DIR/bootleg_data_prep/generate_slices.py \
   --data_dir $BOOTLEG_PREP_WIKIPEDIA_DIR/data/wiki_dump \
   --processes $BOOTLEG_PREP_PROCESS_COUNT_MAX \
   --emb_dir $BOOTLEG_PREP_WIKIPEDIA_DIR/embs \
-  --ent_desc qid2desc_092921.json \
-  --kg_adj kg_adj_092921.txt \
-  --kg_triples kg_triples_092921.json \
+  --ent_desc qid2desc_1129.json \
+  --kg_adj kg_adj_1129.txt \
+  --kg_triples kg_triples_1129.json \
   --hy_vocab ''\
   --hy_types ''\
   --kg_vocab $BOOTLEG_PREP_CODE_DIR/bootleg_data_prep/utils/param_files/pid_names_$BOOTLEG_LANG_CODE.json \
-  --wd_vocab wikidatatitle_to_typeid_092921.json \
-  --wd_types wikidata_types_092921.json \
-  --rel_vocab relation_to_typeid_092921.json \
-  --rel_types kg_relation_types_092921.json
+  --wd_vocab wikidatatitle_to_typeid_1129.json \
+  --wd_types wikidata_types_1129.json \
+  --rel_vocab relation_to_typeid_1129.json \
+  --rel_types kg_relation_types_1129.json
